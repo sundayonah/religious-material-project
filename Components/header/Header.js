bf01320b8c9593fb789ad9b5ad23fb0c22917d39
sundayonah <sundayonah94@gmail.com>
2023-09-21 10:51:32 +0100

    first commit

---
+++ Components/header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Style from './Header.module.css';
+
+const Header = () => {
+   //    const menuItems = ['Home', 'Books', 'MP3', 'MP4', 'Song/Ministration'];
+   const menuItems = [
+      { name: 'Home', url: '../../app/home.js' },
+      { name: 'Books', url: './Home.js' },
+      { name: 'MP3', url: './Home.js' },
+      { name: 'MP4', url: './Home.js' },
+      { name: 'Song/Ministration', url: './Home.js' },
+   ];
+
+   return (
+      <main className={Style.header}>
+         <div>
+            <h1>RM</h1>
+         </div>
+
+         <div className={Style.menu}>
+            {menuItems.map((item, i) => (
+               <ul key={i + 1}>
+                  <li>{item.name}</li>
+                  <a href={item.url} target="_blank"></a>
+               </ul>
+            ))}
+         </div>
+         <div>
+            <span>Connect</span>
+         </div>
+      </main>
+   );
+};
+
+export default Header;

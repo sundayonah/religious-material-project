754f9c6216e3c8943a22c109c068457056073def
sundayonah <sundayonah94@gmail.com>
2023-10-10 08:45:53 +0100

    fix: the toogle menu items

--- src/Context/ReligiousContext.js
+++ src/Context/ReligiousContext.js
@@ -94,6 +94,181 @@ export const StateContextProvider = ({ children }) => {
       }
    };
 
+   // const connectWallet = async () => {
+   //    try {
+   //       if (window.ethereum) {
+   //          const accounts = await window.ethereum.request({
+   //             method: 'eth_requestAccounts',
+   //          });
+
+   //          const provider = new ethers.providers.Web3Provider(window.ethereum);
+   //          const signer = provider.getSigner();
+
+   //          setIsLoading(true);
+
+   //          const messageHash = ethers.utils.hashMessage(
+   //             'Sign-in to web3 kigdom-coin e-commerce'
+   //          );
+   //          const signature = await signer.signMessage(messageHash);
+
+   //          // Save the signature in local storage
+   //          localStorage.setItem('userSignature', signature);
+
+   //          console.log(signature);
+
+   //          const userSignature = localStorage.getItem('userSignature');
+
+   //          console.log(userSignature);
+
+   //          if (userSignature) {
+   //             // Make a POST request to authenticate the user
+   //             const res = await axios.post(
+   //                'http://kingdomcoin-001-site1.ctempurl.com/api/Account/AuthenticateUser',
+   //                {
+   //                   address: accounts[0], // Use the user's address
+   //                   signature: userSignature, // Use the user's signature
+   //                },
+   //                {
+   //                   headers: {
+   //                      'Content-Type': 'application/json',
+   //                   },
+   //                }
+   //             );
+
+   //             console.log(res);
+
+   //             if (res.status === 200) {
+   //                const responseData = res.data.data;
+
+   //                // Store responseData in localStorage
+   //                localStorage.setItem(
+   //                   'responseData',
+   //                   JSON.stringify(responseData)
+   //                );
+
+   //                // Now you can access it later by parsing it back
+   //                const storedData = JSON.parse(
+   //                   localStorage.getItem('responseData')
+   //                );
+
+   //                console.log(responseData);
+
+   //                // You can use storedData as needed in your application
+   //             } else {
+   //                console.error(
+   //                   `Authentication request failed with status code ${res.status}`
+   //                );
+   //                if (res.status === 401) {
+   //                   console.error(
+   //                      'Unauthorized: Check your authorization token.'
+   //                   );
+   //                }
+   //             }
+   //          } else {
+   //             console.error('User signature not found in local storage');
+   //          }
+   //          setIsLoading(false);
+   //       } else {
+   //          console.error('MetaMask not installed');
+   //       }
+   //    } catch (error) {
+   //       console.error('Error signing in with message hash:', error);
+   //    }
+   // };
+
+   // const connectWallet = async () => {
+   //    try {
+   //       if (window.ethereum) {
+   //          const accounts = await window.ethereum.request({
+   //             method: 'eth_requestAccounts',
+   //          });
+
+   //          const provider = new ethers.providers.Web3Provider(window.ethereum);
+   //          const signer = provider.getSigner();
+
+   //          setIsLoading(true);
+
+   //          const messageHash = ethers.utils.hashMessage(
+   //             'Sign-in to web3 kigdom-coin e-comerce'
+   //          );
+   //          const signature = await signer.signMessage(messageHash);
+
+   //          // Save the signature in local storage
+   //          localStorage.setItem('userSignature', signature);
+
+   //          const userSignature = localStorage.getItem('userSignature');
+
+   //          if (userSignature) {
+   //             // Include the signature in the request header
+
+   //             const authURL =
+   //                'http://kingdomcoin-001-site1.ctempurl.com/api/Account/AuthenticateUser';
+
+   //             const res = await axios.post(
+   //                authURL,
+   //                {
+   //                   address: accounts[0], // Use the user's address
+   //                   signature: userSignature, // Use the user's signature
+   //                },
+   //                {
+   //                   headers: {
+   //                      'Content-Type': 'application/json',
+   //                   },
+   //                }
+   //             );
+
+   //             if (res.data.statusCode === 200) {
+   //                // Store responseData in localStorage
+   //                const responseData = res.data.data;
+   //                localStorage.setItem(
+   //                   'responseData',
+   //                   JSON.stringify(responseData)
+   //                );
+   //                // Now you can access it later by parsing it back
+   //                const storedData = JSON.parse(
+   //                   localStorage.getItem('responseData')
+   //                );
+   //             } else {
+   //                console.error(
+   //                   `API request failed with status code ${res.status}`
+   //                );
+   //                if (res.status === 401) {
+   //                   console.error(
+   //                      'Unauthorized: Check your authorization token.'
+   //                   );
+   //                }
+   //             }
+   //          } else {
+   //             console.error('User signature not found in local storage');
+   //          }
+   //          setIsLoading(false);
+   //       } else {
+   //          console.error('MetaMask not installed');
+   //       }
+   //    } catch (error) {
+   //       console.error('Error signing in with message hash:', error);
+   //    }
+   // };
+
+   // const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+   // console.log(wallet);
+   // if (wallet) {
+   //    if (wallet[0]) {
+   //       const ethersProvider = new ethers.providers.Web3Provider(
+   //          wallet[0].provider,
+   //          'any'
+   //       );
+
+   //       const signer = ethersProvider.getSigner();
+
+   //       console.log(signer);
+   //    } else {
+   //       console.log(
+   //          "Wallet is not connected yet or doesn't have a selected address."
+   //       );
+   //    }
+   // }
+
    return (
       <StateContext.Provider
          value={{
--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,17 @@
 import React, { useContext, useState } from 'react';
 import Link from 'next/link';
 import { StateContext } from '@/Context/ReligiousContext';
-import ConnectButton from './connectWallet';
+import { ConnectButton, Menu } from './connectWallet';
 import { ethers } from 'ethers';
 import axios from 'axios';
-
-// import { useAccount, useConnect, useDisconnect } from 'wagmi';
-// import { InjectedConnector } from 'wagmi/connectors/injected';
-// import { useWeb3Modal, useWeb3ModalTheme } from '@web3modal/wagmi/react';
 
 const Header = () => {
    const { address, disconnect, connect, connectWallet } =
       useContext(StateContext);
 
-   // const [message, setMessage] = useState('');
-   // const [signature, setSignature] = useState('');
    const [accounts, setAccounts] = useState('');
    const [isLoading, setIsLoading] = useState(false);
+   const [isMenuOpen, setIsMenuOpen] = useState(false); // State to control menu visibility
 
    // console.log(address);
 
@@ -30,202 +25,141 @@ const Header = () => {
       { name: 'Download', url: '/download' },
    ];
 
-   // const connectWallet = async () => {
-   //    try {
-   //       if (window.ethereum) {
-   //          const accounts = await window.ethereum.request({
-   //             method: 'eth_requestAccounts',
-   //          });
+   const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+   };
 
-   //          const provider = new ethers.providers.Web3Provider(window.ethereum);
-   //          const signer = provider.getSigner();
-
-   //          setIsLoading(true);
-
-   //          const messageHash = ethers.utils.hashMessage(
-   //             'Sign-in to web3 kigdom-coin e-commerce'
-   //          );
-   //          const signature = await signer.signMessage(messageHash);
-
-   //          // Save the signature in local storage
-   //          localStorage.setItem('userSignature', signature);
-
-   //          console.log(signature);
-
-   //          const userSignature = localStorage.getItem('userSignature');
-
-   //          console.log(userSignature);
-
-   //          if (userSignature) {
-   //             // Make a POST request to authenticate the user
-   //             const res = await axios.post(
-   //                'http://kingdomcoin-001-site1.ctempurl.com/api/Account/AuthenticateUser',
-   //                {
-   //                   address: accounts[0], // Use the user's address
-   //                   signature: userSignature, // Use the user's signature
-   //                },
-   //                {
-   //                   headers: {
-   //                      'Content-Type': 'application/json',
-   //                   },
-   //                }
-   //             );
-
-   //             console.log(res);
-
-   //             if (res.status === 200) {
-   //                const responseData = res.data.data;
-
-   //                // Store responseData in localStorage
-   //                localStorage.setItem(
-   //                   'responseData',
-   //                   JSON.stringify(responseData)
-   //                );
-
-   //                // Now you can access it later by parsing it back
-   //                const storedData = JSON.parse(
-   //                   localStorage.getItem('responseData')
-   //                );
-
-   //                console.log(responseData);
-
-   //                // You can use storedData as needed in your application
-   //             } else {
-   //                console.error(
-   //                   `Authentication request failed with status code ${res.status}`
-   //                );
-   //                if (res.status === 401) {
-   //                   console.error(
-   //                      'Unauthorized: Check your authorization token.'
-   //                   );
-   //                }
-   //             }
-   //          } else {
-   //             console.error('User signature not found in local storage');
-   //          }
-   //          setIsLoading(false);
-   //       } else {
-   //          console.error('MetaMask not installed');
-   //       }
-   //    } catch (error) {
-   //       console.error('Error signing in with message hash:', error);
-   //    }
-   // };
-
-   // const connectWallet = async () => {
-   //    try {
-   //       if (window.ethereum) {
-   //          const accounts = await window.ethereum.request({
-   //             method: 'eth_requestAccounts',
-   //          });
-
-   //          const provider = new ethers.providers.Web3Provider(window.ethereum);
-   //          const signer = provider.getSigner();
-
-   //          setIsLoading(true);
-
-   //          const messageHash = ethers.utils.hashMessage(
-   //             'Sign-in to web3 kigdom-coin e-comerce'
-   //          );
-   //          const signature = await signer.signMessage(messageHash);
-
-   //          // Save the signature in local storage
-   //          localStorage.setItem('userSignature', signature);
-
-   //          const userSignature = localStorage.getItem('userSignature');
-
-   //          if (userSignature) {
-   //             // Include the signature in the request header
-
-   //             const authURL =
-   //                'http://kingdomcoin-001-site1.ctempurl.com/api/Account/AuthenticateUser';
-
-   //             const res = await axios.post(
-   //                authURL,
-   //                {
-   //                   address: accounts[0], // Use the user's address
-   //                   signature: userSignature, // Use the user's signature
-   //                },
-   //                {
-   //                   headers: {
-   //                      'Content-Type': 'application/json',
-   //                   },
-   //                }
-   //             );
-
-   //             if (res.data.statusCode === 200) {
-   //                // Store responseData in localStorage
-   //                const responseData = res.data.data;
-   //                localStorage.setItem(
-   //                   'responseData',
-   //                   JSON.stringify(responseData)
-   //                );
-   //                // Now you can access it later by parsing it back
-   //                const storedData = JSON.parse(
-   //                   localStorage.getItem('responseData')
-   //                );
-   //             } else {
-   //                console.error(
-   //                   `API request failed with status code ${res.status}`
-   //                );
-   //                if (res.status === 401) {
-   //                   console.error(
-   //                      'Unauthorized: Check your authorization token.'
-   //                   );
-   //                }
-   //             }
-   //          } else {
-   //             console.error('User signature not found in local storage');
-   //          }
-   //          setIsLoading(false);
-   //       } else {
-   //          console.error('MetaMask not installed');
-   //       }
-   //    } catch (error) {
-   //       console.error('Error signing in with message hash:', error);
-   //    }
-   // };
-
-   // const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-   // console.log(wallet);
-   // if (wallet) {
-   //    if (wallet[0]) {
-   //       const ethersProvider = new ethers.providers.Web3Provider(
-   //          wallet[0].provider,
-   //          'any'
-   //       );
-
-   //       const signer = ethersProvider.getSigner();
-
-   //       console.log(signer);
-   //    } else {
-   //       console.log(
-   //          "Wallet is not connected yet or doesn't have a selected address."
-   //       );
-   //    }
-   // }
+   const closeMenu = () => {
+      setIsMenuOpen(false);
+   };
 
    return (
-      <main className="flex justify-around items-center fixed left-0 top-0 w-full bg-opacity-10 backdrop-blur-md shadow-md h-16">
-         <div className="">
-            <span className="text-white bold">LOGO</span>
-         </div>
+      <>
+         {isMenuOpen && (
+            <div
+               className="fixed inset-0 bg-black opacity-50"
+               onClick={toggleMenu}
+            ></div>
+         )}
+         <main className="flex justify-between px-6  items-center fixed left-0 top-0 w-full bg-opacity-10 backdrop-blur-md shadow-md h-16">
+            <div className="">
+               <span className="text-white bold">LOGO</span>
+            </div>
+            {/* <div className="flex">
+               {menuItems.map((item, i) => (
+                  <ul key={i + 1}>
+                     <Link className="pr-5 text-white" href={item.url}>
+                        {item.name}
+                     </Link>
+                  </ul>
+               ))}
+            </div>
 
-         <div className="flex">
-            {menuItems.map((item, i) => (
-               <ul key={i + 1}>
-                  {/* <li>{item.name}</li> */}
-                  <Link className="pr-5 text-white" href={item.url}>
-                     {item.name}
-                  </Link>
-               </ul>
-            ))}
-         </div>
+            <button onClick={() => connectWallet()}>connect</button>
 
-         <button onClick={() => connectWallet()}>connect</button>
-
-         <ConnectButton />
-      </main>
+            <ConnectButton />
+            <div>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-white "
+               >
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+               </svg>
+            </div> */}{' '}
+            {/* Hide on screens larger than 'md' */}
+            <div className="md:hidden ">
+               {isMenuOpen ? (
+                  ''
+               ) : (
+                  // <div className="md:hidden">
+                  //    <button onClick={toggleMenu} className="text-white">
+                  //       <svg
+                  //          xmlns="http://www.w3.org/2000/svg"
+                  //          fill="none"
+                  //          viewBox="0 0 24 24"
+                  //          strokeWidth={1.5}
+                  //          stroke="currentColor"
+                  //          className="w-6 h-6"
+                  //       >
+                  //          <path
+                  //             strokeLinecap="round"
+                  //             strokeLinejoin="round"
+                  //             d="M6 18L18 6M6 6l12 12"
+                  //          />
+                  //       </svg>
+                  //    </button>
+                  // </div>
+                  <button onClick={toggleMenu} className="text-white">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                     >
+                        <path
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        />
+                     </svg>
+                  </button>
+               )}
+            </div>
+            {/* Menu */}
+            <div
+               className={`${
+                  isMenuOpen ? 'block px-5 py-4 shadow-custom' : 'hidden'
+               } md:flex  absolute right-0  justify-center items-center top-0 w-[50%] md:static md:space-x-4 md:bg-transparent md:w-auto`}
+            >
+               <div className="flex justify-end">
+                  {isMenuOpen && (
+                     <button
+                        onClick={closeMenu}
+                        className="text-white md:hidden"
+                     >
+                        <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           strokeWidth={1.5}
+                           stroke="currentColor"
+                           className="w-6 h-6"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6 18L18 6M6 6l12 12"
+                           />
+                        </svg>
+                     </button>
+                  )}
+               </div>
+               {menuItems.map((item, i) => (
+                  <ul key={i + 1} className={isMenuOpen ? 'pt-3' : ''}>
+                     <Link
+                        onClick={closeMenu}
+                        className="pr-5 text-white"
+                        href={item.url}
+                     >
+                        {item.name}
+                     </Link>
+                  </ul>
+               ))}
+               <button onClick={() => connectWallet()}>connect</button>
+               <ConnectButton />
+            </div>
+         </main>
+      </>
    );
 };
 
